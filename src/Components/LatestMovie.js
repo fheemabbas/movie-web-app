fc8f1b7b9a4bb3d9ef0d0f41bd1f31eab54bc183
FAHIMABBAS HARIYANI <fheemabbas@gmail.com>
2021-02-12 15:30:46 +0530

    getting data

--- src/Components/LatestMovie.js
+++ src/Components/LatestMovie.js
@@ -1,12 +1,28 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getLatestMovie } from "./Action/Main";
+import MovieCard from "./MovieCard";
+const styles = {
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+};
 function Latest(props) {
   useEffect(() => {
     props.getLatestMovie();
   }, []);
   console.log("data:", props.latest);
-  return <div className="App">Latest</div>;
+  return (
+    <div style={styles.container}>
+      {/* Rendering Latest */}
+      {/* {props.latest &&
+        props.latest.data.map((u, i) => {
+          return <MovieCard data={u} key={u.id} />;
+        })} */}
+    </div>
+  );
 }
 const mapStateToProps = (state) => {
   return {
--- src/Components/NowPlayingMovie.js
+++ src/Components/NowPlayingMovie.js
@@ -1,12 +1,30 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getNowPlayingMovie } from "./Action/Main";
+import MovieCard from "./MovieCard";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+};
 function NowPlayng(props) {
   useEffect(() => {
     props.getNowPlayingMovie();
   }, []);
   console.log("data:", props.now_playing);
-  return <div className="App">NowPlayng</div>;
+  return (
+    <div style={styles.container}>
+      {/* Rendering Latest */}
+      {props.now_playing &&
+        props.now_playing.results &&
+        props.now_playing.results.map((u, i) => {
+          return <MovieCard data={u} key={u.id} />;
+        })}
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
